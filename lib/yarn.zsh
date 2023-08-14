113f532acc38799288b87bb082b8593c42fd8622
muhmuhammad_alziqri <muhammad_alziqri@linkaja.id>
2023-08-14 20:37:09 +0700

    feat: add pnpm info

--- lib/yarn.zsh
+++ lib/yarn.zsh
@@ -1,5 +1,21 @@
 function yarn_info() {
   which yarn &>/dev/null || return
   local yarn_prompt=${$(yarn -v)#v}
-  echo "yarn v${ZSH_THEME_NVM_PROMPT_PREFIX}${yarn_prompt}${ZSH_THEME_NVM_PROMPT_SUFFIX}"
+
+  if [[ "${yarn_prompt//[0-9\.]/}" ]]; then
+    return
+  fi
+
+  echo " | yarn v${ZSH_THEME_NVM_PROMPT_PREFIX}${yarn_prompt}${ZSH_THEME_NVM_PROMPT_SUFFIX}"
+}
+
+function pnpm_info() {
+  which pnpm &>/dev/null || return
+  local pnpm_prompt=${$(pnpm -v)#v}
+
+  if [[ "${pnpm_prompt//[0-9\.]/}" ]]; then
+    return
+  fi
+
+  echo " | pnpm v${ZSH_THEME_NVM_PROMPT_PREFIX}${pnpm_prompt}${ZSH_THEME_NVM_PROMPT_SUFFIX}";
 }
